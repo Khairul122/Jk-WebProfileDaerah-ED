abbc18c1d1c49608cc3c4b88c2f15e9ad2ca9cbc
SarahNelviandri <sarah.210170236@mhs.unimal.ac.id>
2024-02-18 06:37:27 +0700

    menambahkan gambar pada objek wisata

--- application/views/wisata.php
+++ application/views/wisata.php
@@ -5,13 +5,13 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title><?= strtoupper($this->db->get("profile")->row()->nama); ?> | OFFICIAL</title>
+    <title><?=strtoupper($this->db->get("profile")->row()->nama);?> | OFFICIAL</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="<?= base_url('assets/images/' . $this->db->get("profile")->row()->logo) ?>" rel="icon">
-    <link href="<?= base_url('assets/images/' . $this->db->get("profile")->row()->logo) ?>" rel="apple-touch-icon">
+    <link href="<?=base_url('assets/images/' . $this->db->get("profile")->row()->logo)?>" rel="icon">
+    <link href="<?=base_url('assets/images/' . $this->db->get("profile")->row()->logo)?>" rel="apple-touch-icon">
 
     <!-- Vendor CSS Files -->
     <link href="assets/vendors/aos/aos.css" rel="stylesheet">
@@ -45,84 +45,91 @@
     <section id="topbar" class="d-flex align-items-center">
         <div class="container d-flex justify-content-center justify-content-md-between">
             <div class="contact-info d-flex align-items-center">
-                <?= $this->db->get("profile")->row()->visi; ?>
+                <?=$this->db->get("profile")->row()->visi;?>
             </div>
             <div class="social-links d-none d-md-flex align-items-center">
-                <a href="<?= base_url("auth") ?>"><i class="bi bi-lock"></i></a>
+                <a href="<?=base_url("auth")?>"><i class="bi bi-lock"></i></a>
             </div>
         </div>
     </section>
     <header id="header" class="d-flex align-items-center">
         <div class="container d-flex align-items-center justify-content-between">
 
-            <h1 class="logo"><a href="<?= base_url() ?>"><?= strtoupper($this->db->get("profile")->row()->nama) ?></a></h1>
+            <h1 class="logo"><a href="<?=base_url()?>"><?=strtoupper($this->db->get("profile")->row()->nama)?></a></h1>
             <!-- Uncomment below if you prefer to use an image logo -->
             <!-- <a href="index.html" class="logo"><img src="assets/images/mycomp.png" alt=""></a> -->
 
             <nav id="navbar" class="navbar">
                 <ul>
-                    <li><a class="nav-link scrollto" href="<?= base_url(); ?>">BERANDA</a></li>
+                    <li><a class="nav-link scrollto" href="<?=base_url();?>">BERANDA</a></li>
                     <li class="dropdown"><a href="#"><span>PROFIL</span> <i class="bi bi-chevron-down"></i></a>
                         <ul>
-                            <li><a href="<?= base_url("sejarah") ?>">SEJARAH</a></li>
-                            <li><a href="<?= base_url("visi") ?>">VISI DAN MISI</a></li>
-                            <!-- <li><a href="<?= base_url("lambang") ?>">LAMBANG DAERAH</a></li> -->
-                            <li><a href="<?= base_url("struktur") ?>">STRUKTUR ORGANISASI</a></li>
-                            <!-- <li><a href="<?= base_url("opd") ?>">OPD</a></li> -->
+                            <li><a href="<?=base_url("sejarah")?>">SEJARAH</a></li>
+                            <li><a href="<?=base_url("visi")?>">VISI DAN MISI</a></li>
+                            <!-- <li><a href="<?=base_url("lambang")?>">LAMBANG DAERAH</a></li> -->
+                            <li><a href="<?=base_url("struktur")?>">STRUKTUR ORGANISASI</a></li>
+                            <!-- <li><a href="<?=base_url("opd")?>">OPD</a></li> -->
                         </ul>
                     </li>
                     <li class="dropdown"><a href="#"><span>PUBLIKASI</span> <i class="bi bi-chevron-down"></i></a>
                         <ul>
-                            <li><a href="<?= base_url("berita"); ?>">BERITA</a></li>
-                            <li><a href="<?= base_url("pengumuman"); ?>">PENGUMUMAN</a></li>
-                            <li><a href="<?= base_url("agenda"); ?>">AGENDA</a></li>
-                            <li><a href="<?= base_url("gallery"); ?>">GALERI</a></li>
+                            <li><a href="<?=base_url("berita");?>">BERITA</a></li>
+                            <li><a href="<?=base_url("pengumuman");?>">PENGUMUMAN</a></li>
+                            <li><a href="<?=base_url("agenda");?>">AGENDA</a></li>
+                            <li><a href="<?=base_url("gallery");?>">GALERI</a></li>
                         </ul>
                     </li>
-                    <li><a class="nav-link scrollto" href="<?= base_url() ?>#contact">KONTAK</a></li>
-                    <li><a class="nav-link scrollto" href="<?= base_url("wisata") ?>">OBJEK WISATA</a></li>
+                    <li><a class="nav-link scrollto" href="<?=base_url()?>#contact">KONTAK</a></li>
+                    <li><a class="nav-link scrollto" href="<?=base_url("wisata")?>">OBJEK WISATA</a></li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav>
         </div>
     </header>
     <main id="main">
-        <section id="wisata">
-            <div id="map"></div>
+    <section id="wisata">
+    <div id="map"></div>
+    <!-- Script untuk Leaflet.js dan konfigurasi peta -->
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <style>
 
-            <!-- Script untuk Leaflet.js dan konfigurasi peta -->
-            <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-            <script>
-                document.addEventListener('DOMContentLoaded', function() {
-                    // Inisialisasi peta Leaflet dengan koordinat terakhir atau Monas jika tidak ada data
-                    var map = L.map('map').setView([-1.1751, 100.6650], 12); // Koordinat pertama atau lokasi default
+        .centered-image {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Inisialisasi peta Leaflet dengan koordinat terakhir atau Monas jika tidak ada data
+            var map = L.map('map').setView([-1.1751, 100.6650], 12); // Koordinat pertama atau lokasi default
 
-                    // Tambahkan layer OpenStreetMap sebagai latar belakang peta
-                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    }).addTo(map);
+            // Tambahkan layer OpenStreetMap sebagai latar belakang peta
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
 
-                    // Tambahkan marker untuk Jembatan Akar
-                    L.marker([-1.181808, 100.600919]).addTo(map)
-                        .bindPopup('<b>Jembatan Akar</b><br>Jembatan akar adalah jembatan yang terbentuk dari jalinan dua akar pohon yang tumbuh berseberangan dan membentang di atas aliran Batang Bayang di kecamatan Bayang Utara, kabupaten Pesisir Selatan, Sumatera Barat. Dalam bahasa Minang, jembatan yang letaknya sekitar 88 km sebelah selatan kota Padang ini oleh masyarakat dinamakan titian aka. Jembatan ini memiliki panjang 25 meter dan lebar 1,5 meter dengan ketinggian dari permukaan sungai sekitar 10 meter. Pengerjaannya dimulai sejak 1890 di bawah pimpinan seorang tokoh masyarakat bernama Pakiah Sokan yang berasal dari Lubuk Silau. Proses merajut akar menjadi jembatan membutuhkan waktu lebih kurang 26 tahun dan jembatan dapat digunakan pada 1916. Kondisinya semakin lama semakin kuat karena semakin besarnya akar pohon beringin yang membentuknya.');
+            // Tambahkan marker untuk Jembatan Akar
+            L.marker([-1.181808, 100.600919]).addTo(map)
+                .bindPopup('<b>Jembatan Akar</b><br><img src="gambar1.jpg" alt="Jembatan Akar" width="300" height="100"  class="centered-image" ><br>Jembatan akar adalah jembatan yang terbentuk dari jalinan dua akar pohon yang tumbuh berseberangan dan membentang di atas aliran Batang Bayang di kecamatan Bayang Utara, kabupaten Pesisir Selatan, Sumatera Barat. Dalam bahasa Minang, jembatan yang letaknya sekitar 88 km sebelah selatan kota Padang ini oleh masyarakat dinamakan titian aka. Jembatan ini memiliki panjang 25 meter dan lebar 1,5 meter dengan ketinggian dari permukaan sungai sekitar 10 meter. Pengerjaannya dimulai sejak 1890 di bawah pimpinan seorang tokoh masyarakat bernama Pakiah Sokan yang berasal dari Lubuk Silau. Proses merajut akar menjadi jembatan membutuhkan waktu lebih kurang 26 tahun dan jembatan dapat digunakan pada 1916. Kondisinya semakin lama semakin kuat karena semakin besarnya akar pohon beringin yang membentuknya.');
 
-                    // Tambahkan marker untuk Rumah Nagari Pohon Atas
-                    L.marker([-1.148872, 100.671643]).addTo(map)
-                        .bindPopup('<b>Rumah Nagari Pohon Atas</b><br>Salah satu destinasi wisata yang terkenal di Nagari Pancuang Taba kecamatan puluik-puluik selatan ini ialah Rumah Pohon yang mendapat julukan Rumah Pohon Negeri di Atas Awan. Julukan tersebut diberikan karena lokasi rumah pohon yang berada di atas perbukitan atau dataran tinggi yang tertutup oleh awan setiap pagi atau sore. Sepanjang perjalanan menuju rumah pohon tersebut, kita akan disuguhkan dengan pemandangan alam yang sangat indah. Mulai dari sawah yang terbentang luas nan hijau, aliran sungai yang jernih, hingga beberapa permukiman warga.');
+            // Tambahkan marker untuk Rumah Nagari Pohon Atas
+            L.marker([-1.148872, 100.671643]).addTo(map)
+                .bindPopup('<b>Rumah Nagari Pohon Atas</b><br><img src="gambar2.jpg" alt="Rumah Nagari Pohon Atas" width="300" height="100" class="centered-image" ><br>Salah satu destinasi wisata yang terkenal di Nagari Pancuang Taba kecamatan puluik-puluik selatan ini ialah Rumah Pohon yang mendapat julukan Rumah Pohon Negeri di Atas Awan. Julukan tersebut diberikan karena lokasi rumah pohon yang berada di atas perbukitan atau dataran tinggi yang tertutup oleh awan setiap pagi atau sore. Sepanjang perjalanan menuju rumah pohon tersebut, kita akan disuguhkan dengan pemandangan alam yang sangat indah. Mulai dari sawah yang terbentang luas nan hijau, aliran sungai yang jernih, hingga beberapa permukiman warga.');
 
-                    // Tambahkan marker untuk Air Terjun Bayang Sani
-                    L.marker([-1.223776, 100.567594]).addTo(map)
-                        .bindPopup('<b>Air Terjun Bayang Sani</b><br>Kabupaten Pesisir Selatan terkenal dengan sebutan Negeri Sejuta Pesona. Benar saja, sepanjang mata memandang menuju lokasi wisata Air Terjun Bayang Sani dipenuhi dengan view alam yang sangat indah. Banyak yang belum tahu, bahwa Air Terjun Bayang Sani memiliki air terjun sebanyak 7 tingkatan dengan lokasi yang berbeda. Namun air terjun yang bisa dikunjungi hanya sampai tingkatan ke tiga saja, karena belum tersedia akses jalan menuju tingkat ke empat sampai ke tingkat ke tujuh.');
+            // Tambahkan marker untuk Air Terjun Bayang Sani
+            L.marker([-1.223776, 100.567594]).addTo(map)
+                .bindPopup('<b>Air Terjun Bayang Sani</b><br><img src="gambar3.jpg" alt="Air Terjun Bayang Sani" width="300" height="100" class="centered-image" ><br>Kabupaten Pesisir Selatan terkenal dengan sebutan Negeri Sejuta Pesona. Benar saja, sepanjang mata memandang menuju lokasi wisata Air Terjun Bayang Sani dipenuhi dengan view alam yang sangat indah. Banyak yang belum tahu, bahwa Air Terjun Bayang Sani memiliki air terjun sebanyak 7 tingkatan dengan lokasi yang berbeda. Namun air terjun yang bisa dikunjungi hanya sampai tingkatan ke tiga saja, karena belum tersedia akses jalan menuju tingkat ke empat sampai ke tingkat ke tujuh.');
 
-                    // Simpan koordinat terakhir ke local storage saat peta digeser
-                    map.on('moveend', function() {
-                        var currentLocation = map.getCenter();
-                        localStorage.setItem('lastLocation', JSON.stringify([currentLocation.lat, currentLocation.lng]));
-                    });
-                });
-            </script>
+            // Simpan koordinat terakhir ke local storage saat peta digeser
+            map.on('moveend', function() {
+                var currentLocation = map.getCenter();
+                localStorage.setItem('lastLocation', JSON.stringify([currentLocation.lat, currentLocation.lng]));
+            });
+        });
+    </script>
+</section>
 
-        </section>
 
     </main>
 
@@ -131,28 +138,28 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-4 col-md-6 footer-contact">
-                        <h3><?= ucwords($this->db->get("profile")->row()->nama); ?></h3>
+                        <h3><?=ucwords($this->db->get("profile")->row()->nama);?></h3>
                         <p>
-                            <?= ucwords($this->db->get("profile")->row()->kota); ?><br><br>
-                            <strong>Phone:</strong> <?= $this->db->get("profile")->row()->telp; ?><br>
-                            <strong>Email:</strong> <?= $this->db->get("profile")->row()->email; ?><br>
+                            <?=ucwords($this->db->get("profile")->row()->kota);?><br><br>
+                            <strong>Phone:</strong> <?=$this->db->get("profile")->row()->telp;?><br>
+                            <strong>Email:</strong> <?=$this->db->get("profile")->row()->email;?><br>
                         </p>
                     </div>
                     <div class="col-lg-4 col-md-6 footer-link">
                         <h4>Statistik Website</h4>
                         <small>
-                            Pengunjung Hari ini : <?= $pengunjunghariini ?> Orang<br>
-                            Total Pengunjung&nbsp;&nbsp;&nbsp;&nbsp;: <?= $totalpengunjung ?> Orang<br>
-                            Pengunjung Online : <?= $pengunjungonline ?> Orang<br>
+                            Pengunjung Hari ini : <?=$pengunjunghariini?> Orang<br>
+                            Total Pengunjung&nbsp;&nbsp;&nbsp;&nbsp;: <?=$totalpengunjung?> Orang<br>
+                            Pengunjung Online : <?=$pengunjungonline?> Orang<br>
                         </small>
                     </div>
                     <div class="col-lg-4 col-md-6 footer-links">
                         <h4>Kunjungi Juga Media Sosial Kami</h4>
                         <p>Silakan pilih media sosial yang anda ini ikuti</p>
                         <div class="social-links mt-3">
-                            <a href="<?= $this->db->get("profile")->row()->twitter ?>" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
-                            <a href="<?= $this->db->get("profile")->row()->facebook ?>" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
-                            <a href="<?= $this->db->get("profile")->row()->instagram ?>" class="instagram" target="_blank"><i class="bx bxl-instagram"></i></a>
+                            <a href="<?=$this->db->get("profile")->row()->twitter?>" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="<?=$this->db->get("profile")->row()->facebook?>" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
+                            <a href="<?=$this->db->get("profile")->row()->instagram?>" class="instagram" target="_blank"><i class="bx bxl-instagram"></i></a>
                         </div>
                     </div>
                 </div>
@@ -161,7 +168,7 @@
 
         <div class="container py-4">
             <div class="copyright">
-                &copy; Copyright <strong><span><?= ucwords($this->db->get("profile")->row()->nama); ?></span></strong>. All Rights Reserved
+                &copy; Copyright <strong><span><?=ucwords($this->db->get("profile")->row()->nama);?></span></strong>. All Rights Reserved
             </div>
             <div class="credits">
 
